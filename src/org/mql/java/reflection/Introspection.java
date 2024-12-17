8c2f340262dbcc08ef0304cc2fd679a15918124f
benzariya saida <benzariyasaida@gmail.com>
2024-12-17 19:05:58 +0100

    first-commit

---
+++ src/org/mql/java/reflection/Introspection.java
@@ -0,0 +1,18 @@
+package org.mql.java.reflection;
+
+
+
+public class Introspection {
+	
+	public Introspection() throws ClassNotFoundException {
+		
+	}
+
+
+	public static void main(String[] args) {
+	    	 
+		
+		
+	}
+
+}
---
+++ src/org/mql/java/reflection/PckageExploerer.java
@@ -0,0 +1,17 @@
+package org.mql.java.reflection;
+
+
+public class PckageExploerer {
+	
+	public PckageExploerer() {
+		
+	}
+	
+
+	
+	public static void main(String[] args) {
+		new PckageExploerer();
+	}
+	
+}
+ 
